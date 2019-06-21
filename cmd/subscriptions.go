16f090c8eb0cb2d276d47e5665a5c394a9b6e86c
Carlos Valiente <carlos@pepelabs.net>
2019-06-21 18:17:21 +0100

    Subscriptions.

--- cmd/subscriptions.go
+++ cmd/subscriptions.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 )
@@ -13,6 +15,7 @@ func init() {
 	createSubscriptionCmd.Flags().StringVar(&ackLevel, "amqp-ack-level", "none", "AMQP ack level")
 	rootCmd.AddCommand(createSubscriptionCmd)
 	rootCmd.AddCommand(deleteSubscriptionCmd)
+	rootCmd.AddCommand(listSubscriptionsCmd)
 }
 
 var createSubscriptionCmd = &cobra.Command{
@@ -44,7 +47,27 @@ var deleteSubscriptionCmd = &cobra.Command{
 		name := args[0]
 		err := client.RGWDeleteSubscription(name)
 		if err != nil {
-			glog.Fatalf("failed to delere subscription %s: %v", name, err)
+			glog.Fatalf("failed to delete subscription %s: %v", name, err)
+		}
+	},
+}
+
+var listSubscriptionsCmd = &cobra.Command{
+	Use:   "list-subscriptions",
+	Args:  cobra.ExactArgs(0),
+	Short: "List all subscriptions",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := getClientOrDie()
+
+		subs, err := client.RGWGetSubscriptions()
+		if err != nil {
+			glog.Fatalf("failed to list subscriptions: %v", err)
+		}
+
+		for _, ss := range subs.Subscriptions {
+			for _, s := range ss.Subscription {
+				fmt.Printf("%s (user: %s, topic: %s)\n", s, ss.Topic.User, ss.Topic.Name)
+			}
 		}
 	},
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/carletes/rgw-pubsub
 
 require (
-	github.com/aws/aws-sdk-go v1.20.4 // indirect
-	github.com/ceph/rgw-pubsub-api v0.0.0-20190620154759-37a9f3ca1ff9
+	github.com/aws/aws-sdk-go v1.20.5 // indirect
+	github.com/ceph/rgw-pubsub-api v0.0.0-20190621130932-e01a48a89774
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/spf13/cobra v0.0.5
 )
